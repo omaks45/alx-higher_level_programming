a2c60e435d4985fa0f7895c8feed57445370058e
omaks45 <danielgozie5@gmail.com>
2022-09-27 07:30:44 -0700

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    a = len(my_list)
+    for i in my_list:
+        if idx < a:
+            return none
+        elif idx > a:
+            return none
+        else:
+            return (my_list(i))
