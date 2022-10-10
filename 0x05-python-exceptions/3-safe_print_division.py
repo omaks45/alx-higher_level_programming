0160076a86f1d43e914147d90f3ca24ab6d00ff2
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-10-10 18:32:43 +0100

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    """returns the division of a by b."""
+    """Returns the division of a by b."""
     try:
-        mod = (a / b)
-    except(ZeroDivisionError, TypeError):
-        mod = none
+        mod = a / b
+    except (TypeError, ZeroDivisionError):
+        mod = None
     finally:
-        print("inside result: {}".format(mod))
-    return mod
+        print("Inside result: {}".format(mod))
+    return (mod)
