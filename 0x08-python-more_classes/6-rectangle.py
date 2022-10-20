2af9b4dfaf3e05d48aa373ccf9586bc4af590ca5
omaks45 <danielgozie5@gmail.com>
2022-10-20 08:43:42 -0700

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""A class that defines a rectangle"""
+"""Class that defines a rectangle"""
 
 
 class Rectangle:
     """this represents a rectangle"""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initializing this rectangle class
@@ -16,6 +17,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -66,3 +68,12 @@ class Rectangle:
             if column < self.__height - 1:
                 rectangle += "\n"
         return (rectangle)
+
+    def __repr__(self):
+        """returns a string representation of the rectangle"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """prints a message for every object that is deleted"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
