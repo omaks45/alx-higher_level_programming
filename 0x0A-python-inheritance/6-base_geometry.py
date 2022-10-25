36842962e2118d6bc9bd503a0b9018e3ca92ad1e
omaks45 <danielgozie5@gmail.com>
2022-10-25 08:43:44 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module containing a BaseGeomtry class"""
+
+
+class BaseGeometry:
+    """A representation of BaseGeometry class"""
+
+    def area(self):
+        """method yet to be implemented"""
+        raise Exception("area() is not yet implemented")
