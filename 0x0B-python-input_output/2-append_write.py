a0ec908b0404e2ea193e3924910cf4b29c8530ea
omaks45 <danielgozie5@gmail.com>
2022-10-27 09:21:17 -0700

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module that appends a string to a file"""
+
+
+def append_write(filename="", text=""):
+    """appending a string to a utf-8 text file"""
+    with open(filename, 'a', encoding="utf-8") as x:
+        return x.append(text)
