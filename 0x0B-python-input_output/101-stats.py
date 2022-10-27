f8414e6960a2dddc4ab6a40e5eb28a4974b5ad48
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2022-10-27 21:59:23 +0100

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,68 +1,40 @@
-ds from standard input and computes metrics
-
+#!/usr/bin/python3
+"""Reads from standard input and computes metrics
 """
 
 
-
-
-
 def print_stats(size, status_codes):
-
     """Print accumulated metrics
-
     """
-
     print("File size: {}".format(size))
-
     for key in sorted(status_codes):
-
         print("{}: {}".format(key, status_codes[key]))
 
 
-
-
-
 if __name__ == "__main__":
-
     import sys
 
-
-
     size = 0
-
     status_codes = {}
-
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-
     count = 0
 
-
-
     try:
-
         for line in sys.stdin:
-
             if count == 10:
-
                 print_stats(size, status_codes)
-
                 count = 1
-
             else:
-
                 count += 1
-
-
 
             line = line.split()
 
-
-
             try:
-
                 size += int(line[-1])
             except (IndexError, ValueError):
-                pass            try:
+                pass
+
+            try:
                 if line[-2] in valid_codes:
                     if status_codes.get(line[-2], -1) == -1:
                         status_codes[line[-2]] = 1
@@ -70,7 +42,9 @@ if __name__ == "__main__":
                         status_codes[line[-2]] += 1
             except IndexError:
                 pass
+
         print_stats(size, status_codes)
+
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
