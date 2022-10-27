4c0338129b8cb62d405ec113d67958ebfa7cce0c
omaks45 <danielgozie5@gmail.com>
2022-10-27 12:33:05 -0700

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module that defines class student"""
+
+
+def __init__(self, first_name, last_name, age):
+    """initialization of class student"""
+
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+    return self.__dict__
