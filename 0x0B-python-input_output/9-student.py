843d0d93bb812a9ffcd70c997649fac19596bec4
omaks45 <danielgozie5@gmail.com>
2022-10-27 12:39:47 -0700

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,4 +9,6 @@ def __init__(self, first_name, last_name, age):
     self.last_name = last_name
     self.age = age
 
-    return self.__dict__
+    def to_json(self):
+        """Retrieving dictionary representation of the student"""
+        return self.__dict__
