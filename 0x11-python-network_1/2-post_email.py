21c125c86c2948ca0c79ca49a75c43f530175799
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-02-03 16:53:13 +0100

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" Sends a POST request to a given URL with a given email.
+"""Sends a POST request to a given URL with a given email.
 Usage: ./2-post_email.py <URL> <email>
- - Displays the body of the response.
-  """
+  - Displays the body of the response.
+"""
 import sys
-import urllib.request
 import urllib.parse
+import urllib.request
 
-if __name__ == "main":
+
+if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
