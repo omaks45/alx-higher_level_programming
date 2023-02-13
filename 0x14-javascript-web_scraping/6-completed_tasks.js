85d544c4864b22b208bb6fe6992a78fe14ed4a91
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-02-13 02:05:11 +0100

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,41 +1,23 @@
-t request = require('request');
+#!/usr/bin/node
 
+const request = require('request');
 const url = process.argv[2];
 
-
-
 request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
 
-	  if (error) {
-
-		      console.log(error);
-
-		      return;
-
-		    }
-
-
-
-	  const tasksCompleted = {};
-
-	  body.forEach((todo) => {
-
-		      if (todo.completed) {
-
-			            if (!tasksCompleted[todo.userId]) {
-
-					            tasksCompleted[todo.userId] = 1;
-
-					          } else {
-
-							          tasksCompleted[todo.userId] += 1;
-
-							        }
-
-			          }
-
-		    });
-
-	  console.log(tasksCompleted);
-
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
 });
