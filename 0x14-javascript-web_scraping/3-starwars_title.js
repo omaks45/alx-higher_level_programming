16f90c74693801eb791bbe1cc92f6ee07fcbe827
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-02-13 01:13:47 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, (error, response, body) => {
-  if(error){
+  if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(body);
