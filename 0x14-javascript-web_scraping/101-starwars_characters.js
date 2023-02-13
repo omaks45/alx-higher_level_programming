1907792d1e983c566bc727d9b13858ba9144594e
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-02-13 02:11:26 +0100

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,63 +1,34 @@
-t request = require('request');
+#!/usr/bin/node
 
-
+const request = require('request');
 
 const movieId = process.argv[2];
-
 const url = `https://swapi.dev/api/films/${movieId}/`;
-
 let characters = [];
 
-
-
 request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
 
-	  if (error) {
-
-		      console.log(error);
-
-		      return;
-
-		    }
-
-
-
-	  const data = JSON.parse(body);
-
-	  characters = data.characters;
-
-	  getCharacters(0);
-
+  const data = JSON.parse(body);
+  characters = data.characters;
+  getCharacters(0);
 });
 
-
-
 const getCharacters = (index) => {
+  if (index === characters.length) {
+    return;
+  }
 
-	  if (index === characters.length) {
-
-		      return;
-
-		    }
-
-
-
-	  request(characters[index], (error, response, body) => {
-
-		      if (error) {
-
-			            console.log(error);
-
-			            return;
-
-			          }
-
-		      const characterData = JSON.parse(body);
-
-		      console.log(characterData.name);
-
-		      getCharacters(index + 1);
-
-		    });
-
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+    getCharacters(index + 1);
+  });
 };
